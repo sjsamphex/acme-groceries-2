706d0bd9dc84c8a69587574b006df53d45978bd7
sjsamphex <sjsamphex@gmail.com>
2020-12-16 09:07:07 -0800

    addition of applymiddleware of redux logger and redux thunk

--- src/store.js
+++ src/store.js
@@ -1,25 +1,30 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 
 const initialState = {
   groceries: [],
-  view: ''
+  view: '',
 };
-const store = createStore((state = initialState, action)=> {
-  if(action.type === 'LOAD'){
-    state = {...state, groceries: action.groceries };
+const store = createStore((state = initialState, action) => {
+  if (action.type === 'LOAD') {
+    state = { ...state, groceries: action.groceries };
   }
-  if(action.type === 'UPDATE'){
-    state = {...state, groceries: state.groceries.map(grocery => grocery.id === action.grocery.id ? action.grocery : grocery )};
+  if (action.type === 'UPDATE') {
+    state = {
+      ...state,
+      groceries: state.groceries.map((grocery) =>
+        grocery.id === action.grocery.id ? action.grocery : grocery
+      ),
+    };
   }
-  if(action.type === 'CREATE'){
-    state = {...state, groceries: [...state.groceries, action.grocery ]}
+  if (action.type === 'CREATE') {
+    state = { ...state, groceries: [...state.groceries, action.grocery] };
   }
-  if(action.type === 'SET_VIEW'){
-    state = {...state, view: action.view};
+  if (action.type === 'SET_VIEW') {
+    state = { ...state, view: action.view };
   }
   return state;
-});
+}, applyMiddleware(logger, thunk));
 
 export default store;
-
-
